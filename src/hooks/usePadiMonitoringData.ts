d231b865bac9de6964187fc773748a546c0f8104
jokopranoto <jokopranoto6104@gmail.com>
2025-06-04 08:08:56 +0700

    update hook Ubinan Padi dan dashboard

--- src/hooks/usePadiMonitoringData.ts
+++ src/hooks/usePadiMonitoringData.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // src/hooks/usePadiMonitoringData.ts
 import { useState, useEffect } from 'react';
 import { createClientComponentSupabaseClient } from '@/lib/supabase';
